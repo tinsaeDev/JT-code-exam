f8886b20af42dadd46f71016f29dbe35916c4e60
Tinsae B <tinsaedev@gmail.com>
2023-05-04 15:13:30 +0300

    Comments  index added

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Post;
+
 class CommentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index( Post $post)
     {
-        //
+        return $post->comments;
     }
 
     /**
